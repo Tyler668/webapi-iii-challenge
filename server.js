17fa8eaa06635b7df3c91326feb486d7a83f29b0
Tyler Gilchrist <tyler668@gmail.com>
2019-10-03 12:39:54 -0400

    no gate

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ server.use(helmet());
 server.use(express.json());
 
 
-server.use('/api/users', gate, userRouter);
+server.use('/api/users', userRouter);
 
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
+
 
 //custom middleware
 
